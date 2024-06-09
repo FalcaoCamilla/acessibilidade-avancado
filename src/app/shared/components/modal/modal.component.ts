de9110552dcdf44beb6099ac93257747736180a2
Camilla Falcão <millafalcao.acad@gmail.com>
2024-06-09 02:09:10 -0300

    Feature | Modal - Criação do ModalComponent

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { ModalConfig } from "./interfaces/modal-config";
 
 @Component({
   selector: 'app-modal',
@@ -7,5 +8,6 @@ import { Component } from "@angular/core";
 })
 
 export class ModalComponent {
+  public config: ModalConfig;
 
 }
