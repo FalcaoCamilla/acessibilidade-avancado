6ae65546c69bfa98d0d01469f775ce8ad30b2169
Camilla Falcão <millafalcao.acad@gmail.com>
2024-06-10 22:57:31 -0300

    Feature | FocusBack - Funcionamento de diretiva focusBack

--- src/app/shared/directives/focus-back/focus-back.directive.ts
+++ src/app/shared/directives/focus-back/focus-back.directive.ts
@@ -1,9 +1,22 @@
-import { Directive } from "@angular/core";
+import { Directive, OnDestroy, OnInit } from "@angular/core";
 
 @Directive({
   selector: '[appFocusBack]'
 })
 
-export class FocusBackDirective {
+export class FocusBackDirective implements OnInit, OnDestroy {
+  private _lastFocusedElement: Element;
 
+  public ngOnInit(): void {
+    /*
+     * O document.activeElemente é uma referência para o elemento com focus dentro do documento (página) exibida.
+     */
+    this._lastFocusedElement = document.activeElement;
+  }
+
+  public ngOnDestroy(): void {
+    if(this._lastFocusedElement) {
+      (this._lastFocusedElement as HTMLElement).focus()
+    }
+  }
 }
