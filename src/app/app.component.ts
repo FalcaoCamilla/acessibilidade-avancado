2fc9bb714d404ab834d497c1a958716d4dcd6dbf
Camilla Falcão <millafalcao.acad@gmail.com>
2024-06-10 23:32:41 -0300

    Feature | App - Inclusão de reactiveForms com validação no modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,46 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { ModalService } from './shared/components/modal/services/modal.service';
 import { ModalRef } from './shared/components/modal/models/model-ref';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
 
   title = 'a11y-p2';
   public firstName = 'Camilla';
   public modalRef: ModalRef;
 
-  constructor(private _modalService: ModalService) {}
+  public form: FormGroup;
+
+  constructor(
+    private _fb: FormBuilder,
+    private _modalService: ModalService
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this._fb.group({
+      firstName: ['Camilla', Validators.required],
+      surname: ['', Validators.required],
+      age: ['', Validators.required],
+      info: [false]
+    })
+  }
+
   public show(): void {
     this.modalRef = this._modalService.open({
       templateRef: this.modalTemplateRef,
       title: 'User details'
     })
+  }
+
+  public submit() {
+    if(this.form.invalid) {
+      return
+    }
   }
 }
