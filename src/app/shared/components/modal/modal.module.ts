8de19a320489c44de0a99ec120f190adc1cfe1dd
Camilla Falcão <millafalcao.acad@gmail.com>
2024-06-10 22:45:53 -0300

    Refactor | ModalModule - Importação da diretiva focusTrap

--- src/app/shared/components/modal/modal.module.ts
+++ src/app/shared/components/modal/modal.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FocusTrapModule } from '../../directives/focus-trap/focus-trap.module';
 import { ModalComponent } from "./modal.component";
 import { ModalService } from "./services/modal.service";
 
 @NgModule({
   declarations: [ModalComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FocusTrapModule
+  ],
   exports: [ModalComponent],
   providers: [ModalService]
 })
