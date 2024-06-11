3d8b7707f1f657b375fd5a6dac898a6c0481f0fd
Camilla Falcão <millafalcao.acad@gmail.com>
2024-06-10 22:50:48 -0300

    Refactor | ModalModule - Importação de diretiva FocusBack

--- src/app/shared/components/modal/modal.module.ts
+++ src/app/shared/components/modal/modal.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { FocusTrapModule } from '../../directives/focus-trap/focus-trap.module';
 import { ModalComponent } from "./modal.component";
 import { ModalService } from "./services/modal.service";
+import { FocusBackModule } from '../../directives/focus-back/focus-back.module';
 
 @NgModule({
   declarations: [ModalComponent],
   imports: [
     CommonModule,
-    FocusTrapModule
+    FocusTrapModule,
+    FocusBackModule
   ],
   exports: [ModalComponent],
   providers: [ModalService]
