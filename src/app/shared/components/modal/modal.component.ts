8f08ca6bfbd94ab775991e6272f00174d3ae50ff
Camilla Falcão <millafalcao.acad@gmail.com>
2024-06-10 22:20:16 -0300

    Refactor | Modal - Refatoração para importação da classe ModalRef

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,13 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, HostBinding } from "@angular/core";
 import { ModalConfig } from "./interfaces/modal-config";
+import { fade } from "../../animations/fade";
+import { ModalRef } from "./models/model-ref";
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
+  animations: [fade]
 })
 
 export class ModalComponent {
+  /*
+   * Sempre que o componente for renderizado, o elemento host terá a animação de "fade in" ativada.
+   */
+  @HostBinding('@fade') fade = true;
+  public modalRef: ModalRef;
   public config: ModalConfig;
 
 }
